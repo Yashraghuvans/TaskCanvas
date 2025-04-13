4ca3c2c931d7da8854a06403cec1c8260a401196
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-14 01:07:11 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,19 @@
+"use client"
+import React from 'react'
+import Login from './Components/Auth/Login'
+import EmployeeDashboard from './Components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './Components/Dashboard/AdminDashboard'
+
+const page = () => {
+  return (
+    <>
+      <div className='text-white'>
+        {/* <Login /> */}
+        {/* <EmployeeDashboard /> */}
+        {/* <AdminDashboard /> */}
+      </div>
+    </>
+  )
+}
+
+export default page
