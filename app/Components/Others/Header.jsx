969ccae2318f24e5d339355d2637a1a9db084bc0
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-14 01:07:41 +0530

    Create Header.jsx

---
+++ app/Components/Others/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <div className='flex items-end justify-between text-white'>
+            <h1 className='text-xl font-medium'>Hello<br/><span className='text-3xl font-semibold'>Yash Raghuvanshi 👋</span></h1>
+            <button className='bg-red-600 rounded-lg px-5 py-3 text-white'>Log Out</button>
+        </div>
+    </>
+  )
+}
+
+export default Header
