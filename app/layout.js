60755bf185c5833b829bd28a7c758cc9e80d8ec2
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-14 13:25:58 +0530

    fragmented with context api

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import AuthContext from "./Context/AuthContext";
+import TaskContext from "./Context/TaskContext";
 import "./globals.css";
 
 export const metadata = {
@@ -11,7 +12,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <AuthContext>
-          {children}
+          <TaskContext>
+            {children}
+          </TaskContext>
         </AuthContext>
       </body>
     </html>
