d5742761fe2edee5db365d0bf8889bca00818574
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-14 13:26:22 +0530

    creating local storage for data sotring

---
+++ app/Utils/localStorage.jsx
@@ -0,0 +1,243 @@
+const employees = [
+    {
+        "id": 1,
+        "email": "employee1@example.com",
+        "password": "123",
+        "tasks": [
+            {
+                "taskId": 101,
+                "taskTitle": "Design Database Schema",
+                "taskDescription": "Design the database schema for the new user management system.",
+                "taskDate": "2024-07-20",
+                "category": "Engineering",
+                "active": true,
+                "newTask": true,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 102,
+                "taskTitle": "Implement Authentication",
+                "taskDescription": "Implement user authentication and authorization using JWT.",
+                "taskDate": "2024-07-27",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+             {
+                "taskId": 103,
+                "taskTitle": "Write Unit Tests",
+                "taskDescription": "Write unit tests for authentication service.",
+                "taskDate": "2024-07-29",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "email": "employee2@example.com",
+        "password": "123",
+        "tasks": [
+            {
+                "taskId": 201,
+                "taskTitle": "Develop REST API",
+                "taskDescription": "Develop RESTful API endpoints for user data retrieval.",
+                "taskDate": "2024-07-22",
+                "category": "Engineering",
+                "active": true,
+                "newTask": true,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 202,
+                "taskTitle": "Integrate with Frontend",
+                "taskDescription": "Integrate the API with the frontend user interface.",
+                "taskDate": "2024-07-29",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 203,
+                "taskTitle": "Optimize API Performance",
+                "taskDescription": "Optimize the API for better performance and scalability.",
+                "taskDate": "2024-08-05",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+             {
+                "taskId": 204,
+                "taskTitle": "Implement Caching",
+                "taskDescription": "Implement Redis caching for frequently accessed data..",
+                "taskDate": "2024-08-07",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "email": "employee3@example.com",
+        "password": "123",
+        "tasks": [
+            {
+                "taskId": 301,
+                "taskTitle": "Design UI Components",
+                "taskDescription": "Design reusable UI components for the dashboard.",
+                "taskDate": "2024-07-23",
+                "category": "Engineering",
+                "active": true,
+                "newTask": true,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 302,
+                "taskTitle": "Develop User Interface",
+                "taskDescription": "Develop the user interface for the messaging dashboard.",
+                "taskDate": "2024-07-30",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+             {
+                "taskId": 303,
+                "taskTitle": "Fix Layout Issues",
+                "taskDescription": "Fix the layout issues reported by QA.",
+                "taskDate": "2024-08-01",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "email": "employee4@example.com",
+        "password": "123",
+        "tasks": [
+            {
+                "taskId": 401,
+                "taskTitle": "Set up CI/CD Pipeline",
+                "taskDescription": "Set up a CI/CD pipeline using Jenkins for automated deployment.",
+                "taskDate": "2024-07-24",
+                "category": "Engineering",
+                "active": true,
+                "newTask": true,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 402,
+                "taskTitle": "Configure Servers",
+                "taskDescription": "Configure the production servers for the application.",
+                "taskDate": "2024-08-01",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+             {
+                "taskId": 403,
+                "taskTitle": "Monitor Deployment",
+                "taskDescription": "Monitor the application deployment and resolve any issues.",
+                "taskDate": "2024-08-02",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 404,
+                "taskTitle": "Write Deployment Scripts",
+                "taskDescription": "Create the scripts for automating the deployment process.",
+                "taskDate": "2024-07-26",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "email": "employee5@example.com",
+        "password": "123",
+        "tasks": [
+            {
+                "taskId": 501,
+                "taskTitle": "Write Documentation",
+                "taskDescription": "Write API documentation for the messaging dashboard.",
+                "taskDate": "2024-07-25",
+                "category": "Engineering",
+                "active": true,
+                "newTask": true,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 502,
+                "taskTitle": "Create User Manual",
+                "taskDescription": "Create a user manual for the messaging dashboard application.",
+                "taskDate": "2024-08-02",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+             {
+                "taskId": 503,
+                "taskTitle": "Prepare Release Notes",
+                "taskDescription": "Prepare release notes for the upcoming version.",
+                "taskDate": "2024-08-03",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            },
+            {
+                "taskId": 504,
+                "taskTitle": "Review Documentation",
+                "taskDescription": "Review the documentation for technical accuracy.",
+                "taskDate": "2024-08-06",
+                "category": "Engineering",
+                "active": true,
+                "newTask": false,
+                "completed": false,
+                "failed": false
+            }
+        ]
+    }
+];
+
+const admin = [
+    {
+        "id": 101,
+        "email": "admin1@example.com",
+        "password": "123"
+    }
+]
