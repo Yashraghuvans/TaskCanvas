ae666c2b891d068154afe50f9349540582754c4a
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-14 13:24:45 +0530

    making the task list for now only
    just for demo

---
+++ app/Components/TaskList/TaskList.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const TaskList = () => {
+    return (
+        <>
+            <div id='tasklist' className='h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full py-5  mt-10 overflow-x-auto'>
+                <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl py-5 px-5'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h2>
+                        <h2 className='text-sm'>20 April 2025</h2>
+                    </div>
+                    <h2 className='text-2xl font-semibold mt-5'>Make a react project</h2>
+                    <p className='text-sm mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea autem temporibus modi tempore repellat nemo!</p>
+                </div>
+                <div className='flex-shrink-0 h-full w-[300px] bg-green-400 rounded-xl py-5 px-5'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h2>
+                        <h2 className='text-sm'>20 April 2025</h2>
+                    </div>
+                    <h2 className='text-2xl font-semibold mt-5'>Make a react project</h2>
+                    <p className='text-sm mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea autem temporibus modi tempore repellat nemo!</p>
+                </div>
+                <div className='flex-shrink-0 h-full w-[300px] bg-blue-400 rounded-xl py-5 px-5'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h2>
+                        <h2 className='text-sm'>20 April 2025</h2>
+                    </div>
+                    <h2 className='text-2xl font-semibold mt-5'>Make a react project</h2>
+                    <p className='text-sm mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea autem temporibus modi tempore repellat nemo!</p>
+                </div>
+                <div className='flex-shrink-0 h-full w-[300px] bg-orange-400 rounded-xl py-5 px-5'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h2>
+                        <h2 className='text-sm'>20 April 2025</h2>
+                    </div>
+                    <h2 className='text-2xl font-semibold mt-5'>Make a react project</h2>
+                    <p className='text-sm mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea autem temporibus modi tempore repellat nemo!</p>
+                </div>
+                <div className='flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl py-5 px-5'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h2>
+                        <h2 className='text-sm'>20 April 2025</h2>
+                    </div>
+                    <h2 className='text-2xl font-semibold mt-5'>Make a react project</h2>
+                    <p className='text-sm mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea autem temporibus modi tempore repellat nemo!</p>
+                </div>
+                <div className='flex-shrink-0 h-full w-[300px] bg-emerald-400 rounded-xl py-5 px-5'>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h2>
+                        <h2 className='text-sm'>20 April 2025</h2>
+                    </div>
+                    <h2 className='text-2xl font-semibold mt-5'>Make a react project</h2>
+                    <p className='text-sm mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea autem temporibus modi tempore repellat nemo!</p>
+                </div>
+                
+            </div>
+        </>
+    )
+}
+
+export default TaskList
