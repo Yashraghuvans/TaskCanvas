4175169d79c468e0b9ef1c5c6f356895cead24d0
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-15 11:05:34 +0530

    creating call and fetch

--- app/Utils/localStorage.jsx
+++ app/Utils/localStorage.jsx
@@ -241,3 +241,14 @@ const admin = [
         "password": "123"
     }
 ]
+
+export const setLocalStorage = ()=>{
+    localStorage.setItem("employees", JSON.stringify(employees));
+    localStorage.setItem("admin", JSON.stringify(admin));
+}
+
+export const getLocalStorage = ()=>{
+    const employees = JSON.parse(localStorage.getItem("employees"));
+    const admin = JSON.parse(localStorage.getItem("admin"));
+    return{employees,admin};
+}
