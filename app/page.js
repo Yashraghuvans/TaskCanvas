ea81e8e13eb4eddcf8e816a976a5265a68bd43b2
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-15 23:53:23 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,44 +1,41 @@
 "use client"
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './Components/Auth/Login'
 import EmployeeDashboard from './Components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './Components/Dashboard/AdminDashboard'
 
 const page = () => {
   const [user, setUser] = useState(null);
-  const authData=useContext(AuthContext);
 
   useEffect(() => {
-      if(authData){
-        const loggedInUser=localStorage.getItem('loggedInUser');
-        if(loggedInUser){
-          setUser(loggedInUser.role);
-        }
-      }
-  }, [authData]);
-  
+    const loggedInUser = localStorage.getItem('loggedInUser');
+    if (loggedInUser) {
+      setUser(JSON.parse(loggedInUser).role);
+    }
+  }, []);
 
-  const handleLogin = (email,password) => {
-    if(email === 'admin@me.com' && password === '123') {
+  const handleLogin = (email, password) => {
+    if (email === 'admin@me.com' && password === '123') {
       setUser('admin');
-      localStorage.setItem('loggedInUser', JSON.stringify({role:'admin'}));
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }));
     }
-    else if(authData && authData.employees.find((e)=>email==e.email && password==e.password)) {
+    else if(email=="employee@me.com" && password=="123"){
       setUser('employee');
-      localStorage.setItem('loggedInUser', JSON.stringify({role:'employee'}));
-    }
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee' }));
+    } 
     else {
-      alert('Invalid credentials !');
+      alert('Invalid credentials!');
     }
-  }
+  };
+
   return (
     <>
       <div className='text-white'>
-        {!user ? <Login handleLogin={handleLogin}/> : ""}
-        {user=='admin' ? <AdminDashboard /> : <EmployeeDashboard />}
+        {!user ? <Login handleLogin={handleLogin} /> : ""}
+        {user === 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
