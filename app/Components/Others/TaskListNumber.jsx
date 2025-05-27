a4f4aa0a9d37acf606df1a891bd0886ef8721b5a
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-27 18:51:12 +0530

    adding the task categories and functionalities

--- app/Components/Others/TaskListNumber.jsx
+++ app/Components/Others/TaskListNumber.jsx
@@ -5,20 +5,20 @@ const TaskListNumber = () => {
     <>
         <div className='flex justify-between gap-5 screen py-10'>
             <div className='rounded-xl w-[45%] py-6 px-9 bg-red-400'>
-                <h2 className='text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+                <h2 className='text-3xl font-semibold'>4</h2>
+                <h3 className='text-xl font-medium'>Urgency Task</h3>
             </div>
             <div className='rounded-xl w-[45%] py-6 px-9 bg-blue-400'>
-                <h2 className='text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+                <h2 className='text-3xl font-semibold'>6</h2>
+                <h3 className='text-xl font-medium'>Importance Task</h3>
             </div>
             <div className='rounded-xl w-[45%] py-6 px-9 bg-green-400'>
-                <h2 className='text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+                <h2 className='text-3xl font-semibold'>3</h2>
+                <h3 className='text-xl font-medium'>Time Required Task</h3>
             </div>
             <div className='rounded-xl w-[45%] py-6 px-9 bg-yellow-400'>
-                <h2 className='text-3xl font-semibold'>0</h2>
-                <h3 className='text-xl font-medium'>New Task</h3>
+                <h2 className='text-3xl font-semibold'>8</h2>
+                <h3 className='text-xl font-medium'>Community Task</h3>
             </div>
 
         </div>
