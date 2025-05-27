816707b35cd25e1c028a2bbdad82640771710e08
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-27 18:50:32 +0530

    adjusting the size of the block

--- app/Components/Others/AllTask.jsx
+++ app/Components/Others/AllTask.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AllTask = () => {
   return (
     <>
-        <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto'>
+        <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-full overflow-auto'>
             <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
                 <h2>Yash</h2>
                 <h3>Make some react components</h3>
