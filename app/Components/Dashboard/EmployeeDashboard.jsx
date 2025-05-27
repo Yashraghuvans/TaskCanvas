c33a75f34e41e538596016ab219f839881329dc4
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-27 18:12:19 +0530

    apply catch of props

--- app/Components/Dashboard/EmployeeDashboard.jsx
+++ app/Components/Dashboard/EmployeeDashboard.jsx
@@ -3,11 +3,11 @@ import Header from '../Others/Header'
 import TaskListNumber from '../Others/TaskListNumber'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({ handleLogout }) => {
     return (
         <>
             <div className='p-10 bg-[#1C1C1C] h-screen text-white'>
-                <Header />
+                <Header handleLogout={handleLogout} />
                 <TaskListNumber />
                 <TaskList/>
             </div>
