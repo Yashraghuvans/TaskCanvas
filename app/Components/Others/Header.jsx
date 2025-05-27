eacd0b53c223a752f51acbb26405936a5f79b0b2
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-27 18:12:30 +0530

    adding props

--- app/Components/Others/Header.jsx
+++ app/Components/Others/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ handleLogout }) => {
   return (
     <>
         <div className='flex items-end justify-between text-white'>
             <h1 className='text-xl font-medium'>Hello<br/><span className='text-3xl font-semibold'>Yash Raghuvanshi 👋</span></h1>
-            <button className='bg-red-600 rounded-lg px-5 py-3 text-white'>Log Out</button>
+            <button 
+              className='bg-red-600 rounded-lg px-5 py-3 text-white'
+              onClick={handleLogout}
+            >
+              Log Out
+            </button>
         </div>
     </>
   )
