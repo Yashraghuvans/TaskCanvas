9f8bdd471b7e128adfddf96ac59bea4696debc93
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-28 10:31:18 +0530

    updating the style

--- app/Components/Dashboard/EmployeeDashboard.jsx
+++ app/Components/Dashboard/EmployeeDashboard.jsx
@@ -6,7 +6,7 @@ import TaskList from '../TaskList/TaskList'
 const EmployeeDashboard = ({ handleLogout }) => {
     return (
         <>
-            <div className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 text-white">
+            <div className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 text-white px-2 sm:px-0">
                 <Header handleLogout={handleLogout} />
                 <TaskListNumber />
                 <TaskList/>
